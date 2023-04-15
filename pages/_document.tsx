72b3b789c60624c4027be8962d90e2afcc31ca33
Abdullah Alam <alammooo>
2023-04-15 21:33:32 +0700

    add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Abdullah Alam</title>
+        <meta
+          name="description"
+          content="Tech enthusiast with passion for programming, instinct for crafting visually stunning websites that provide exceptional user experiences."
+        />
+        <meta
+          name="google-site-verification"
+          content="Vr28ZS8XZ04quPlFByWRy1AtF-4O3jqfgWR217zhuQQ"
+        />
+        <meta
+          property="og:title"
+          content="Abdullah Alam"
+        />
+        <meta
+          property="og:description"
+          content="Tech enthusiast with passion for programming, instinct for crafting visually stunning websites that provide exceptional user experiences."
+        />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="./favicon.ico"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
