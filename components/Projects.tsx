f7859630c87f6a7cb0d5e95c0582f7db0005873c
Abdullah Alam <alammooo>
2023-04-16 00:22:20 +0700

    add more picture

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,23 +2,22 @@ import { Carousel } from "flowbite-react"
 import SectionTitle from "./utils/SectionTitle"
 import Image from "next/image"
 import { projectImg } from "@/data/main"
+import HeadlessTab from "./utils/HeadlessTab"
 
 export default function Projects() {
   return (
     <section className="">
       <div className="mx-auto max-w-7xl px-10 py-10">
         <SectionTitle type="project" />
-        <div className="mx-auto grid grid-cols-3 gap-10 mt-8">
-          
-            {projectImg.map((el, i) => (
-              <Image
-              className="shadow border"
-                src={el}
-                key={i}
-                alt={`${i}`}
-              />
-            ))}
-          
+        <div className="mx-auto mt-8 grid grid-cols-2 max-w-4xl gap-10">
+          {projectImg.map((el, i) => (
+            <Image
+              className="cursor-pointer border shadow-lg"
+              src={el}
+              key={i}
+              alt={`${i}`}
+            />
+          ))}
         </div>
       </div>
     </section>
